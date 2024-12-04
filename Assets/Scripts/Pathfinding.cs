61c6354e513198af5e2e50215625c9664cb1840e
BerkayBabaoglu <112875768+BerkayBabaoglu@users.noreply.github.com>
2024-12-04 18:17:15 +0300

    script update

--- Assets/Scripts/Pathfinding.cs
+++ Assets/Scripts/Pathfinding.cs
@@ -4,15 +4,48 @@ using UnityEngine;
 
 public class Pathfinding : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    Grid grid;
+
+    private void Awake()
     {
-        
+        grid = GetComponent<Grid>();
     }
 
-    // Update is called once per frame
-    void Update()
+    void FindPath(Vector3 startPos, Vector3 targetPos)
     {
-        
+        Node startNode = grid.NodeFromWorldPoint(startPos);
+        Node targetNode = grid.NodeFromWorldPoint(targetPos);
+
+        List<Node> openSet = new List<Node>();
+        HashSet<Node> closedSet = new HashSet<Node>();
+        openSet.Add(startNode);
+
+        while(openSet.Count > 0)
+        {
+            Node currentNode = openSet[0];
+            for(int i = 1;i<openSet.Count; i++)
+            {
+                if (openSet[i].fCost < currentNode.fCost || openSet[i].fCost == currentNode.fCost && openSet[i].hCost < currentNode.hCost)
+                {
+                    currentNode = openSet[i];
+                }
+            }
+
+            openSet.Remove(currentNode);
+            closedSet.Add(currentNode);
+
+            if(currentNode == targetNode)
+            {
+                return;
+            }
+
+            foreach(Node neighbour in grid.GetNeighbours(currentNode))
+            {
+                if(!neighbour.walkable || closedSet.Contains(neighbour))
+                {
+                    continue;
+                }
+            }
+        }
     }
 }
