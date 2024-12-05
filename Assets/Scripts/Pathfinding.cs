f1cdbe444c0c5e9cec3cfec86319471ba2d889a4
BerkayBabaoglu <112875768+BerkayBabaoglu@users.noreply.github.com>
2024-12-05 16:46:07 +0300

    script update

--- Assets/Scripts/Pathfinding.cs
+++ Assets/Scripts/Pathfinding.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using System.Diagnostics;
 using System.Linq;
+using System;
 
 public class Pathfinding : MonoBehaviour
 {
@@ -99,14 +100,26 @@ public class Pathfinding : MonoBehaviour
             currentNode = currentNode.parent;
         }
         Vector3[] waypoints = SimplifyPath(path);
-        waypoints.Reverse();
+        Array.Reverse(waypoints);
         return waypoints;
 
     }
 
     Vector3[] SimplifyPath(List<Node> path)
     {
+        List<Vector3> waypoints = new List<Vector3>();
+        Vector2 directionOld = Vector2.zero;
 
+        for(int i = 1; i < path.Count; i++)
+        {
+            Vector2 directionNew = new Vector2(path[i - 1].gridX - path[i].gridX, path[i - 1].gridY - path[i].gridY);
+            if(directionNew != directionOld)
+            {
+                waypoints.Add(path[i].worldPosition);
+            }
+            directionOld = directionNew;
+        }
+        return waypoints.ToArray();
     }
 
     int GetDistance(Node nodeA, Node nodeB)
